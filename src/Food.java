d7bf0cb1bbc038dde5083b6d6d762f7c649cd83e
CHONG JHONG YUEN <TP063147@mail.apu.edu.my>
2023-10-16 20:06:43 +0800

    Update 2.0

--- src/Food.java
+++ src/Food.java
@@ -1,9 +1,31 @@
 public class Food {
+    
     private String name;
-    private static int id;
-    private int cost;
+    private static int id = 001;
+    private double cost;
+    
     public Food() {
-
+        id++;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public int getId(){
+        return id;
+    }
+    
+    public void setCost(double cost){
+        this.cost = cost;
+    }
+    
+    public double getCost(){
+        return cost;
     }
 
 }
