14c99329782950b39c50cc0f89595a6abd5a5f69
johan <johan@localhost>
2023-10-16 10:21:34 +0800

    User update

--- src/User.java
+++ src/User.java
@@ -1,2 +1,21 @@
 public class User {
+    private static int userID;
+    private String password;
+    private byte roles;
+    private int balance;
+    private String role_converter;
+
+    public User() {
+
+        switch (roles) {
+            case 1:
+                this.role_converter = "Customer";
+            case 2:
+                this.role_converter = "Vendor";
+            case 3:
+                this.role_converter = "Delivery Runner";
+            case 4:
+                this.role_converter = "Administrator";
+        }
+    }
 }
