450db77298ad9a1d4de66446431dcdacc43f7af9
CHONG JHONG YUEN <TP063147@mail.apu.edu.my>
2023-10-16 20:06:31 +0800

    Update 2.0

--- src/User.java
+++ src/User.java
@@ -1,28 +1,67 @@
 public class User {
-    private static int id;
+    
+    private static int id = 001;
     private String password;
     private byte roles;
-    private int balance;
+    private double balance = 0;
     private String converter;
 
-    public User() {
-
+    public User(byte roles) {
+        id++;
         switch (roles) {
             case 1:
                 this.converter = "Customer";
+                break;
             case 2:
                 this.converter = "Vendor";
+                break;
             case 3:
                 this.converter = "Delivery Runner";
+                break;
             case 4:
                 this.converter = "Administrator";
+                break;
         }
     }
 
     public static int getId() {
         return id;
     }
-    public String converter(){
+    public void setPass(String password){
+        this.password = password;
+    }
+    public String getPass(){
+        return password;
+    }
+    public void addBal(double balance){
+        this.balance += balance;
+    }
+    public void setBal(double balance){
+        this.balance = balance;
+    }
+    public double getBal(){
+        return balance;
+    }
+    
+    public void setRoles(byte roles){
+        this.roles = roles;
+        switch (roles) {
+            case 1:
+                this.converter = "Customer";
+                break;
+            case 2:
+                this.converter = "Vendor";
+                break;
+            case 3:
+                this.converter = "Delivery Runner";
+                break;
+            case 4:
+                this.converter = "Administrator";
+                break;
+        }
+    }
+    
+    public String getRoles(){
         return converter;
     }
 }
