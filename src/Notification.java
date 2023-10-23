8f5047fd6eb77ff2e0e6d825b537af4f018bfe6f
CHONG JHONG YUEN <TP063147@mail.apu.edu.my>
2023-10-23 22:26:20 +0800

    Library Update

--- src/Notification.java
+++ src/Notification.java
@@ -3,11 +3,19 @@ public class Notification { //Incomplete
     private String notification;
     private User user;
 
-    public Notification(String notification) {
+    public Notification(String notification, User user) {
 
     }
 
     public String getUser() {
         return user.getId();
+    }
+
+    public String getNotification() {
+        return notification;
+    }
+
+    public void setNotification(String notification) {
+        this.notification = notification;
     }
 }
