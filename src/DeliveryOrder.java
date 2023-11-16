4ca4f002355732dd26b00ef7130e231da9ec422c
CHONG JHONG YUEN <TP063147@mail.apu.edu.my>
2023-11-16 20:31:23 +0800

    Created Delivery Order class

---
+++ src/DeliveryOrder.java
@@ -0,0 +1,37 @@
+public class DeliveryOrder extends Order{
+    
+    private DeliveryRunner runner;
+    private double deliveryFee = 2.50; //Delivery is for within campus only, thus distance is fixed and delivery fee too
+    private Status statusRunner;
+    
+    public DeliveryOrder(String vendorId, Customer customer, String foodName, String foodId, double foodCost) {
+        super(vendorId, customer, foodName, foodId, foodCost);
+        super.setCost(foodCost + deliveryFee);
+    }
+    
+    public void setRunner (DeliveryRunner runner) { //To set designated runner after found
+        this.runner = runner;
+    }
+    
+    @Override
+    public String getId() { //Order ID is made of vendor ID + customer ID + runner ID (If it was a delivery)
+        return runner.getId() + ":" + super.getId();
+    }
+    
+    public String getRunner() {
+        return runner.getId();
+    }
+    
+    public void setRunnerStatus(Status status) { //For runner to set statuses
+        statusRunner = status;
+    }
+    
+    public void runnerProfit() { //Can only be called if status = DELIVERED, before this object is deleted after saving to history
+        runner.addBal(deliveryFee);
+    }
+    
+    @Override
+    public String toString() { //For writing to order history
+        return runner.getId() + ":" + super.toString();
+    }
+}
